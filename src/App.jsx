28dc738382af362c1d3caaaeea9efc3f11ec0e5c
pablo gutierrez <pabloyucragutierrez@gmail.com>
2024-12-30 20:57:00 -0500

    scroll top

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,42 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import "./App.css";
-import { Header } from "./assets/components/Header";
-import { Inicio } from "./assets/pages/Inicio";
-import { Proyectos } from "./assets/pages/Proyectos";
-import { Cv } from "./assets/pages/Cv";
-import { Contacto } from "./assets/pages/Contacto";
-import { Footer } from "./assets/components/Footer";
-import { Home } from "./assets/pages/Home";
-import Error404 from "./assets/pages/Error404";
-import { PoliticaDePrivacidad } from "./assets/pages/PoliticaDePrivacidad";
+  import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+  import "./App.css";
+  import { Header } from "./assets/components/Header";
+  import { Inicio } from "./assets/pages/Inicio";
+  import { Proyectos } from "./assets/pages/Proyectos";
+  import { Cv } from "./assets/pages/Cv";
+  import { Contacto } from "./assets/pages/Contacto";
+  import { Footer } from "./assets/components/Footer";
+  import { Home } from "./assets/pages/Home";
+  import Error404 from "./assets/pages/Error404";
+  import { PoliticaDePrivacidad } from "./assets/pages/PoliticaDePrivacidad";
+import ScrollToTop from "./assets/components/ScrollToTop";
 
-function App() {
-  return (
-    <div className="container">
-      <BrowserRouter basename="/">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/inicio" element={<Inicio />} />
-            <Route path="/curriculum" element={<Navigate to="/sobre-mi" />} />
-            <Route path="/sobre-mi" element={<Cv />} />
-            <Route path="/cv" element={<Navigate to="/sobre-mi" />} />
-            <Route path="/proyectos" element={<Proyectos />} />
-            <Route path="/contacto" element={<Contacto />} />
-            <Route
-              path="/politica-de-privacidad"
-              element={<PoliticaDePrivacidad />}
-            />
-            <Route path="*" element={<Error404 />} />
-          </Routes>
-        </main>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
-}
+  function App() {
+    return (
+      <div className="container">
+        <BrowserRouter basename="/">
+        <ScrollToTop />
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/inicio" element={<Inicio />} />
+              <Route path="/curriculum" element={<Navigate to="/sobre-mi" />} />
+              <Route path="/sobre-mi" element={<Cv />} />
+              <Route path="/cv" element={<Navigate to="/sobre-mi" />} />
+              <Route path="/proyectos" element={<Proyectos />} />
+              <Route path="/contacto" element={<Contacto />} />
+              <Route
+                path="/politica-de-privacidad"
+                element={<PoliticaDePrivacidad />}
+              />
+              <Route path="*" element={<Error404 />} />
+            </Routes>
+          </main>
+          <Footer />
+        </BrowserRouter>
+      </div>
+    );
+  }
 
-export default App;
+  export default App;
